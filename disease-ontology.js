8297bd5600715a88c3c8cc834e060b42060cad7e
Brian Dunnette <dunn0172@umn.edu>
2014-12-12 16:48:19 -0600

    moving disease update to server method

--- disease-ontology.js
+++ disease-ontology.js
@@ -1,19 +1,45 @@
 Diseases = new Meteor.Collection('diseases');
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    console.log('BioPortal API Key: ' + Meteor.settings.bioportalApiKey);
-    console.log(Diseases.find().count() + ' diseases in database');
-    var diseases = HTTP.get('http://data.bioontology.org/ontologies/DOID/classes', {
-      params: {
-        apikey: Meteor.settings.bioportalApiKey
+  Meteor.methods({
+    updateAllDiseases: function () {
+      var result = HTTP.get(Meteor.settings.bioportal.startUrl, {
+        params: {
+          apikey: Meteor.settings.bioportal.apiKey
+        }
+      });
+
+      for (var d in result.data.collection) {
+        var disease = result.data.collection[d];
+        console.log(disease);
+        Diseases.upsert({
+          '@id': disease['@id']
+        }, disease);
       }
-    });
-    console.log(diseases);
-    for (var d in diseases.data.collection) {
-      var disease = diseases.data.collection[d];
-      console.log(disease);
-      Diseases.insert(disease);
-    };
+
+      for (i = result.data.nextPage; i <= result.data.pageCount; i++) {
+        console.log(i);
+        var result = HTTP.get(Meteor.settings.bioportal.startUrl, {
+          params: {
+            apikey: Meteor.settings.bioportal.apiKey,
+            page: i
+          }
+        });
+        console.log(result);
+        for (var d in result.data.collection) {
+          var disease = result.data.collection[d];
+          console.log(disease);
+          Diseases.upsert({
+            '@id': disease['@id']
+          }, disease);
+        }
+      }
+    },
+  });
+
+  Meteor.startup(function () {
+    console.log(Meteor.settings);
+    console.log('BioPortal API Key: ' + Meteor.settings.bioportal.apiKey);
+    console.log(Diseases.find().count() + ' diseases in database');
   });
 }
