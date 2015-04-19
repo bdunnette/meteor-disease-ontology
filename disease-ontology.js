d6a1d1a9331f6697caec8d0cf928b4f0623e544e
Brian Dunnette <bdunnette@gmail.com>
2015-04-19 13:08:00 -0500

    removing stanza line comments; tidying

--- disease-ontology.js
+++ disease-ontology.js
@@ -1,82 +1,96 @@
 Diseases = new Meteor.Collection('diseases');
 
-var parseStanza = function(stanza, getStanzaType){
-	var stanzaObject = {};
-	stanzaLines = stanza.split("\n");
-	if (getStanzaType === true) {
-	  stanzaObject.type = stanzaLines.shift();
-	}
-	stanzaLines.forEach(function(line){
-    var splitString=line.split(":");
-    var key=splitString.shift();
-    var value=splitString.join(":").replace(/^\s+/,'');
+var parseStanza = function(stanza, getStanzaType) {
+  var stanzaObject = {};
+  stanzaLines = stanza.split("\n");
+  if (getStanzaType === true) {
+    stanzaObject.type = stanzaLines.shift();
+  }
+  stanzaLines.forEach(function(line) {
+    var splitString = line.split(":");
+    var key = splitString.shift();
+    var value = splitString.join(":").replace(/^\s+/, '');
+    if (value.match(/ \! /)){
+  		value=value.split(" ! ")[0];
+  	}
     if (stanzaObject.hasOwnProperty(key) && typeof stanzaObject[key] === 'object') {
       stanzaObject[key].push(value);
-    } else if (stanzaObject.hasOwnProperty(key)) {
+    }
+    else if (stanzaObject.hasOwnProperty(key)) {
       var keyVal = stanzaObject[key];
       stanzaObject[key] = [];
       stanzaObject[key].push(keyVal);
       stanzaObject[key].push(value);
-    } else {
+    }
+    else {
       stanzaObject[key] = value;
     }
   });
-	return stanzaObject;
+  return stanzaObject;
 }
 
 
 if (Meteor.isServer) {
   Meteor.methods({
-    getOBO: function(oboUrl){
-      HTTP.get(oboUrl, function(err, response){
+    getOBO: function(oboUrl) {
+      HTTP.get(oboUrl, function(err, response) {
         console.log(err);
         Diseases.upsert({
-              'oboUrl': oboUrl
-            }, {$set:{
-              'oboContent': response.content,
-              'oboUrl': oboUrl
-            }});
+          'oboUrl': oboUrl
+        }, {
+          $set: {
+            'oboContent': response.content,
+            'oboUrl': oboUrl
+          }
+        });
       });
     },
-    
-    parseStanzas: function(oboUrl){
-      var oboFile = Diseases.findOne({oboUrl: oboUrl});
+
+    parseStanzas: function(oboUrl) {
+      var oboFile = Diseases.findOne({
+        oboUrl: oboUrl
+      });
       var oboContent = oboFile.oboContent;
       var stanzas = oboContent.split("\n\n");
       var ontology = parseStanza(stanzas[0]);
-      
+
       Diseases.update({
-              'oboUrl': oboUrl
-            }, {$set:{
-              'stanzas': stanzas.slice(1)
-            }});
+        'oboUrl': oboUrl
+      }, {
+        $set: {
+          'stanzas': stanzas.slice(1)
+        }
+      });
     },
-    
-    parseHeader: function(oboUrl){
-      var oboFile = Diseases.findOne({oboUrl: oboUrl});
+
+    parseHeader: function(oboUrl) {
+      var oboFile = Diseases.findOne({
+        oboUrl: oboUrl
+      });
       var stanzas = oboFile.stanzas;
       var ontology = parseStanza(stanzas[0]);
-      
+
       Diseases.update({
-              'oboUrl': oboUrl
-            }, {$set:{
-              'ontology': ontology
-            }});
+        'oboUrl': oboUrl
+      }, {
+        $set: {
+          'ontology': ontology
+        }
+      });
     },
-    
-    parseTerms: function(oboUrl){
-      var oboFile = Diseases.findOne({oboUrl: oboUrl});
+
+    parseTerms: function(oboUrl) {
+      var oboFile = Diseases.findOne({
+        oboUrl: oboUrl
+      });
       var stanzas = oboFile.stanzas;
-      var terms = stanzas.slice(0,10);
-      
-      terms.forEach(function(stanza){
+      var terms = stanzas.slice(0, 10);
+
+      terms.forEach(function(stanza) {
         console.log(parseStanza(stanza, true));
       });
     }
-      
+
   });
 
 }
-
-
-
